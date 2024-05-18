4a1d75ce6c941a935fdfdfde0b12b21c6956aae9
Alzhanbaev Kamchybek <aljanbaevkam@gmail.com>
2024-05-18 18:07:14 +0600

    Initial commit

---
+++ src/main/java/kg/project/coffeenets/CoffeeNetsApplication.java
@@ -0,0 +1,13 @@
+package kg.project.coffeenets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeNetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeNetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/project/coffeenets/CoffeeNetsApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.project.coffeenets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeNetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
